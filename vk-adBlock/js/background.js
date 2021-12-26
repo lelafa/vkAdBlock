7331fbd1f685325d2acc5ed2fe3b66ca4bd87311
Aleksei Anikeev <lelafa17@gmail.com>
2021-12-26 14:15:34 +0100

    Update background.js

--- vk-adBlock/js/background.js
+++ vk-adBlock/js/background.js
@@ -1,11 +1,22 @@
-function VkBlock(className, displayState){
-    
-    var elements = document.getElementsByClassName(className)
-    
-    for (var i = 0; i < elements.length; i++){
-        elements[i].style.display = displayState;
+const resizeObserver = new ResizeObserver(entries => {
+    for (let entry of entries) {
+        if(entry.contentBoxSize) {
+            function VkBlock(displayState, ...className ){
+                
+                var elements = document.getElementsByClassName(className)
+                
+                for (var i = 0; i < elements.length; i++){
+                    elements[i].style.display = displayState;
+                    console.log(loop)
+                }
+                console.log('runned')
+
+            }
+
+            VkBlock( 'none', '_ads_block_data_w', 'apps_feedRightAppsBlock__row')
+        }
     }
-
-}
-
-window.onscroll = () => VkBlock('_ads_block_data_w', 'none')
+console.log('Size changed');
+});
+//window.onscroll = () => VkBlock( 'none', '_ads_block_data_w', 'apps_feedRightAppsBlock__row')
+resizeObserver.observe(document.getElementsByClassName('upanel').item(0));
