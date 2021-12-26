095ae19e4fa40ca2919ca0ac2e02e014d2bb5e96
Aleksei Anikeev <lelafa17@gmail.com>
2021-12-26 14:18:54 +0100

    Revert "Update background.js"
    This reverts commit 7331fbd1f685325d2acc5ed2fe3b66ca4bd87311.

--- vk-adBlock/js/background.js
+++ vk-adBlock/js/background.js
@@ -1,22 +1,11 @@
-const resizeObserver = new ResizeObserver(entries => {
-    for (let entry of entries) {
-        if(entry.contentBoxSize) {
-            function VkBlock(displayState, ...className ){
-                
-                var elements = document.getElementsByClassName(className)
-                
-                for (var i = 0; i < elements.length; i++){
-                    elements[i].style.display = displayState;
-                    console.log(loop)
-                }
-                console.log('runned')
-
-            }
-
-            VkBlock( 'none', '_ads_block_data_w', 'apps_feedRightAppsBlock__row')
-        }
+function VkBlock(className, displayState){
+    
+    var elements = document.getElementsByClassName(className)
+    
+    for (var i = 0; i < elements.length; i++){
+        elements[i].style.display = displayState;
     }
-console.log('Size changed');
-});
-//window.onscroll = () => VkBlock( 'none', '_ads_block_data_w', 'apps_feedRightAppsBlock__row')
-resizeObserver.observe(document.getElementsByClassName('upanel').item(0));
+
+}
+
+window.onscroll = () => VkBlock('_ads_block_data_w', 'none')
